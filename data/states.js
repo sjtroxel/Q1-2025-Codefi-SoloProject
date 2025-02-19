8f9292dd46de59d9630d70e6d1c2a8812c67ea57
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-02-19 13:55:59 -0600

    renderStates in JavaScript works!

--- data/states.js
+++ data/states.js
@@ -1,4 +1,4 @@
-export const states = [
+const states = [
     {id: 1,
         name: "Alabama",
     },
---
+++ scripts/states.js
@@ -0,0 +1,25 @@
+function renderStates(liststates) {
+    const statesHTML = liststates
+      .map((state) => `
+        <div class="state-container">
+                <div class="tools">
+                  <div class="circle">
+                    <span class="red box"></span>
+                  </div>
+                  <div class="circle">
+                    <span class="yellow box"></span>
+                  </div>
+                  <div class="circle">
+                    <span class="green box"></span>
+                  </div>
+                </div>
+                <div class="cardContent">${state.name}
+                </div>
+              </div>
+      `)
+      .join("");
+  
+    const statesGrid = document.querySelector('.js-states-grid');
+    statesGrid.innerHTML = statesHTML;
+  }
+  
