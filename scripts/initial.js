eeb559b06a92fdc703e4805252ed36672d1c71b8
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-03-01 13:47:32 -0600

    edit

--- scripts/initial.js
+++ scripts/initial.js
@@ -1,21 +1,21 @@
 import { states } from "../data/states.js";
-import {renderStates, handleGuessState, handleAddToFavorites, handleDeleteFromFavorites} from "./states.js"
+import { renderStates, handleGuessState, handleAddToFavorites, handleDeleteFromFavorites } from "./states.js"
 
 // conditional: if there is something in local storage renderStates(stuff from local strage) otherwise renderstates(original data)
 
-let statesJSON = '';
-
-let statesData = localStorage.getItem('statesJSON');
-    if (statesData) {
-        statesJSON = statesData
-    } else {
-        statesJSON = '';
-        localStorage.setItem("statesJSON", JSON.stringify(statesJSON));
-    }
 
 
+let statesJSON = localStorage.getItem('statesJSON');
+if (statesJSON) {
+    statesJSON = JSON.parse(statesJSON); 
+} else {
+    statesJSON = states;
+    localStorage.setItem("statesJSON", JSON.stringify(statesJSON)); 
+}
 
-renderStates(states);
+
+
+renderStates(statesJSON);
 
 handleGuessState(states)
 
